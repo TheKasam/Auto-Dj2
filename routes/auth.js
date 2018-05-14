86344a0982bba4a20b3b758e0e846214f216fd7c
TheKasam <saikasam98@gmail.com>
2018-05-13 22:16:59 -0500

    added post

--- routes/auth.js
+++ routes/auth.js
@@ -187,5 +187,34 @@ router.get('/callback', function(req, res) {
     }
   });
 
+  router.post('/getToken', function(req, res, next) {
+    //check if user exists
+    User.findOne({_id: req.body.id}, function(err, user) {
+      if (err) {
+          return res.status(500).json({
+              title: 'An error occurred',
+              error: err
+          });
+      }
+      if (!user) {
+          return res.status(401).json({
+              title: 'Login failed',
+              error: {message: 'user not found'}
+          });
+      }
+      // if (!bcrypt.compareSync(req.body.password, user.password)) {
+      //     return res.status(401).json({
+      //         title: 'Login failed',
+      //         error: {message: 'Invalid login credentials'}
+      //     });
+      // }
+      var token = jwt.sign({user: user}, 'secret', {expiresIn: 7200});
+      res.status(200).json({
+          message: 'Successfully logged in',
+          token: token,
+          userId: user._id
+      });
+  });
+});
 
 module.exports = router;
