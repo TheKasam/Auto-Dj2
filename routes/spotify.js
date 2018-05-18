dff32ba2659782a86274a1f7f645c12dae3a9821
Sai Kasam <saikasam98@gmail.com>
2018-05-18 02:00:29 -0500

    passing internal id instaed of user id

--- routes/spotify.js
+++ routes/spotify.js
@@ -3,56 +3,49 @@ var router = express.Router();
 var SpotifyWebApi = require('spotify-web-api-node');
 
 router.get('/getplaylists', function (req, res, next) {
-    console.log("query");
 
     var authId = JSON.parse(req.query.updates[0]).value
-    console.log("Accss id");
-    console.log(authId);
+
     var userId = JSON.parse(req.query.updates[1]).value
 
     var client_id = '356fadb6961741c1ba6aac9966edbcbf'; // Your client id
     var client_secret = 'f3b9982a3e3347bfa60263d1d50fbbc2'; // Your secret
     var redirect_uri = 'http://localhost:3000/login/callback'; // Your redirect uri
+
     var spotifyApi = new SpotifyWebApi({
       clientId: client_id,
       clientSecret: client_secret,
       redirectUri:redirect_uri
     });
+
     spotifyApi.setAccessToken(authId);
- // Get Elvis' albums
-    // Get multiple albums
-    spotifyApi.getAlbums(['3aORS9GEmg02j3cPNAgVdS', '3aORS9GEmg02j3cPNAgVdS'])
-    .then(function(data) {
-      console.log('Albums information', data.body);
-    }, function(err) {
-      console.error(err);
-    });
-    // spotifyApi.getUserPlaylists("6220ecortl2kdhvliq37igyil",{ limit: 10, offset: 20 }, function(err, data) {
+
+    spotifyApi.getUserPlaylists(userId,{ limit: 10, offset: 20 }, function(err, data) {
 
         
 
-    //     if (err) {
-    //       return res.status(500).json({
-    //           title: 'An error occurred',
-    //           error: err
-    //       });
-    //     }
+        if (err) {
+          return res.status(500).json({
+              title: 'An error occurred',
+              error: err
+          });
+        }
+        console.log(data.body);
+        // var playlistsArray = {};
+        // console.log(data);
+        // data["items"].forEach(function(item){
+        //   var itemurl = item["uri"].split(":");
+        //   playlistsArray[item["name"]] = itemurl[4];
+        // });
 
-    //     var playlistsArray = {};
-    //     console.log(data);
-    //     data["items"].forEach(function(item){
-    //       var itemurl = item["uri"].split(":");
-    //       playlistsArray[item["name"]] = itemurl[4];
-    //     });
+        // console.log(playlistsArray);
 
-    //     console.log(playlistsArray);
+        // res.status(200).json({
+        //   message: 'Success',
+        //   obj: playlistsArray
+        // });
 
-    //     res.status(200).json({
-    //       message: 'Success',
-    //       obj: playlistsArray
-    //     });
-
-    // });
+    });
 });
 
 module.exports = router;
