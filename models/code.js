644e5f3b091da524482855a1ed44ed08378aadce
Sai Kasam <saikasam98@gmail.com>
2018-05-19 10:31:05 -0500

    added code model

---
+++ models/code.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var User = require('./user');
+
+var schema = new Schema({
+    name: {type: String, required: true},
+    id:{type: String, required: true},
+    user: {type: Schema.Types.ObjectId, ref: 'User'}
+});
+
+
+module.exports = mongoose.model('Code', schema);
