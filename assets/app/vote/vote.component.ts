d4d32123364b79507a9fe2bbe4d12fe51dbb5f3f
Sai Kasam <saikasam98@gmail.com>
2018-05-19 10:29:38 -0500

    random code generator

--- assets/app/vote/vote.component.ts
+++ assets/app/vote/vote.component.ts
@@ -10,7 +10,21 @@ export class VoteComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.code  = this.randomCodeGenerator()
   }
   
   current_songs = ["molly","fear","psycho"]
+  code = "";
+  randomCodeGenerator(){
+    length = 5
+    var text = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (var i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+  }
+
+
 }
