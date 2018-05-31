7c6b637d656b3258729dd7546196452df8850908
Aman <aj280598@gmail.com>
2018-05-31 14:12:01 -0500

    creation auto dj playlist

--- routes/spotify.js
+++ routes/spotify.js
@@ -144,6 +144,15 @@ router.post('/createDJPlaylist', function(req, res, next){
     console.log(req.body.params.updates[0].value);
     var accesstoken = req.body.params.updates[0].value;
     var token = req.body.params.updates[1].value;
+    var client_id = '356fadb6961741c1ba6aac9966edbcbf'; // Your client id
+    var client_secret = 'f3b9982a3e3347bfa60263d1d50fbbc2'; // Your secret
+    var redirect_uri = 'http://localhost:3000/login/callback'; // Your redirect uri
+
+    var spotifyApi = new SpotifyWebApi({
+        clientId: client_id,
+        clientSecret: client_secret,
+        redirectUri: redirect_uri
+    });
     spotifyApi.setAccessToken(accesstoken);
     spotifyApi.getMe(function(err, data) {
         if (err) {
